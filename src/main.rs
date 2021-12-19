ce8742f7213ce7f6c915fc3b123c5ffb077a2060
quaoz <quaoz@protonmail.com>
2021-12-19 17:05:04 +0000

    init
    Signed-off-by: quaoz <quaoz@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
-use serenity::model::channel::Message;
 use serenity::framework::standard::{
-	StandardFramework,
-	CommandResult,
-	macros::{
-		command,
-		group
-	}
+    macros::{command, group},
+    CommandResult, StandardFramework,
 };
+use serenity::model::channel::Message;
 
+use dotenv::dotenv;
 use std::env;
 
 #[group]
@@ -23,28 +20,32 @@ impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
-	let framework = StandardFramework::new()
-			.configure(|c| c.prefix("~")) // set the bots prefix to "~"
-			.group(&GENERAL_GROUP);
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bots prefix to "~"
+        .group(&GENERAL_GROUP);
 
-	// Login with a bot token from the environment
-	let token = env::var("DISCORD_TOKEN").expect("token");
-	let mut client = Client::builder(token)
-			.event_handler(Handler)
-			.framework(framework)
-			.await
-			.expect("Error creating client");
+    dotenv().ok();
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
 
-	// start listening for events by starting a single shard
-	if let Err(why) = client.start().await {
-		println!("An error occurred while running the client: {:?}", why);
-	}
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 }
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-	msg.reply(ctx, "Pong!").await?;
+    let content = format!(
+        "The bots ping is: {}ms",
+        chrono::Utc::now().timestamp_millis() - msg.timestamp.timestamp_millis()
+    );
+    msg.reply(ctx, content).await?;
 
-	Ok(())
+    Ok(())
 }
-
