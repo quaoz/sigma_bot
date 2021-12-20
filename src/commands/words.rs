8d2d35edc7d917c699685aad9dddf768ec5c85f2
quaoz <quaoz@protonmail.com>
2021-12-20 21:30:47 +0000

    cooler

--- src/commands/words.rs
+++ src/commands/words.rs
@@ -1,33 +1,97 @@
-use std::env;
+use serenity::builder::{CreateEmbed, CreateEmbedAuthor};
 use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use std::env;
 
 #[command]
 #[aliases(def)]
 #[description = "Defines the given words"]
 async fn define(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-	let word = args.single::<String>()?;
+    let word = args.single::<String>()?;
 
-	let token = env::var("DICTIONARY_TOKEN").expect("Expected a token in the environment");
-	let url = format!("https://www.dictionaryapi.com/api/v3/references/collegiate/json/{}?key={}", &word, token);
+    let token = env::var("DICTIONARY_TOKEN").expect("Expected a token in the environment");
+    let url = format!(
+        "https://www.dictionaryapi.com/api/v3/references/collegiate/json/{}?key={}",
+        &word, token
+    );
+    let json: serde_json::Value = reqwest::get(&url).await?.json().await?;
 
-	let response = reqwest::get(url).await?.text().await?;
-	let json: serde_json::Value = serde_json::from_str(&response).expect("JSON was not well-formatted");
+    let entries = json.as_array().unwrap();
+    let mut defs: Vec<&Vec<serde_json::Value>> = vec![json
+        .get(0)
+        .unwrap()
+        .get("shortdef")
+        .unwrap()
+        .as_array()
+        .unwrap()];
 
-	let defs = json.get(0).unwrap().get("shortdef").unwrap().as_array().unwrap();
-	let mut def = String::from(format!("The definition of {} is:\n", word));
+    for i in 1..entries.len() {
+        let value = format!("{}:{}", &word, &i + 1);
+        let pattern = value.as_str();
 
-	let mut index = 1;
-	for item in defs {
-		let mut v: Vec<char> = item.as_str().unwrap().chars().collect();
-		v[0] = v[0].to_uppercase().nth(0).unwrap();
-		let capitalised: String = v.into_iter().collect();
+        if entries
+            .get(i)
+            .unwrap()
+            .get("meta")
+            .unwrap()
+            .get("id")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .starts_with(&pattern)
+        {
+            defs.push(
+                entries
+                    .get(i)
+                    .unwrap()
+                    .get("shortdef")
+                    .unwrap()
+                    .as_array()
+                    .unwrap(),
+            );
+        } else {
+            break;
+        }
+    }
 
-		def.push_str(&*format!("{}. {}\n", index, capitalised ));
-		index += 1;
-	}
+    let mut embed = CreateEmbed::default();
+    embed.title(format!("The definition of {} is:\n", &word));
 
-	msg.reply(&ctx.http, &def).await?;
-	Ok(())
+    let mut entry_number = 1;
+    for def in defs {
+        let mut field = String::from("");
+        let mut index = 1;
+
+        for item in def {
+            let mut v: Vec<char> = item.as_str().unwrap().chars().collect();
+            v[0] = v[0].to_uppercase().nth(0).unwrap();
+            let capitalised: String = v.into_iter().collect();
+
+            field.push_str(&*format!("{}. {}\n", index, capitalised));
+            index += 1;
+        }
+
+        embed.field(&*format!("\nEntry {}:\n", entry_number), field, true);
+        entry_number += 1;
+    }
+
+    let mut auth = CreateEmbedAuthor::default();
+    auth.name(&msg.author.name);
+    auth.url(
+        &msg.author
+            .avatar_url()
+            .unwrap_or(String::from(&msg.author.default_avatar_url())),
+    );
+    msg.channel_id
+        .send_message(&ctx, |f| {
+            f.content("").embed(|e| {
+                e.0 = embed.0;
+                e
+            })
+        })
+        .await
+        .unwrap();
+
+    Ok(())
 }
