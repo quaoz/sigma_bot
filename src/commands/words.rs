a74213be54f25667a9eacc5650d800633681205a
quaoz <quaoz@protonmail.com>
2021-12-20 21:49:22 +0000

    coolerer
    Signed-off-by: quaoz <quaoz@protonmail.com>

--- src/commands/words.rs
+++ src/commands/words.rs
@@ -10,14 +10,18 @@ use std::env;
 async fn define(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let word = args.single::<String>()?;
 
+	// Reads the token and creates the request url
     let token = env::var("DICTIONARY_TOKEN").expect("Expected a token in the environment");
     let url = format!(
         "https://www.dictionaryapi.com/api/v3/references/collegiate/json/{}?key={}",
         &word, token
     );
+
+	// Gets a json response from the url
     let json: serde_json::Value = reqwest::get(&url).await?.json().await?;
 
     let entries = json.as_array().unwrap();
+	// Get the first entry
     let mut defs: Vec<&Vec<serde_json::Value>> = vec![json
         .get(0)
         .unwrap()
@@ -26,10 +30,12 @@ async fn define(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
         .as_array()
         .unwrap()];
 
+	// Get the remaining entries
     for i in 1..entries.len() {
         let value = format!("{}:{}", &word, &i + 1);
         let pattern = value.as_str();
 
+		// Check that the entry is not a similar term
         if entries
             .get(i)
             .unwrap()
@@ -39,7 +45,7 @@ async fn define(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
             .unwrap()
             .as_str()
             .unwrap()
-            .starts_with(&pattern)
+            == pattern
         {
             defs.push(
                 entries
@@ -55,27 +61,46 @@ async fn define(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
         }
     }
 
+	// Create the embed
     let mut embed = CreateEmbed::default();
     embed.title(format!("The definition of {} is:\n", &word));
 
     let mut entry_number = 1;
+	// Iterate through the definitions
     for def in defs {
         let mut field = String::from("");
         let mut index = 1;
 
+		// Iterate through each definitions items
         for item in def {
+			// Capitalise the first letter of each item
             let mut v: Vec<char> = item.as_str().unwrap().chars().collect();
             v[0] = v[0].to_uppercase().nth(0).unwrap();
             let capitalised: String = v.into_iter().collect();
 
-            field.push_str(&*format!("{}. {}\n", index, capitalised));
+            field.push_str(&*format!("{}. {}\n\n", index, capitalised));
             index += 1;
         }
 
-        embed.field(&*format!("\nEntry {}:\n", entry_number), field, true);
+		// Get the word type
+        let word_type = entries
+            .get(entry_number - 1)
+            .unwrap()
+            .get("fl")
+            .unwrap()
+            .as_str()
+            .unwrap();
+
+		// Add the field to the embed
+        embed.field(
+            &*format!("\nEntry {} ({}):\n", entry_number, &word_type),
+            field,
+            true,
+        );
         entry_number += 1;
     }
 
+	// Add the author
     let mut auth = CreateEmbedAuthor::default();
     auth.name(&msg.author.name);
     auth.url(
@@ -83,6 +108,8 @@ async fn define(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
             .avatar_url()
             .unwrap_or(String::from(&msg.author.default_avatar_url())),
     );
+
+	// Send the message
     msg.channel_id
         .send_message(&ctx, |f| {
             f.content("").embed(|e| {
