1f5fef1813ca40a38d5afab00de7bec7eae47d07
quaoz <quaoz@protonmail.com>
2022-03-11 21:05:40 +0000

    wrong bit

--- src/util/poll_utils.rs
+++ src/util/poll_utils.rs
@@ -26,20 +26,20 @@ pub enum ReactionEvent<'a> {
 
 pub fn render_message(poll: &Poll) -> String {
 	let mut message_text = format!("**Poll:** {}\n\n", poll.question);
-	//let total_answerers = poll.answerers.iter().sum::<usize>();
+	let total_answerers = poll.answerers.iter().sum::<usize>();
 
 	for (i, (answer, &num)) in poll.answers.iter().zip(poll.answerers.iter()).enumerate() {
 		let emoji = std::char::from_u32('🇦' as u32 + i as u32).expect("Failed to format emoji");
 		message_text.push(emoji);
 
-		//if total_answerers > 0 {
-		//	let percent = num as f64 / total_answerers as f64 * 100.;
-		//	message_text.push_str(&format!(" {:.0}%", percent));
-		//}
+		if total_answerers > 0 {
+			let percent = num as f64 / total_answerers as f64 * 100.;
+			message_text.push_str(&format!(" {:.0}%", percent));
+		}
 
 		message_text.push(' ');
 		message_text.push_str(answer);
-		message_text.push_str(&format!(" ({} votes)", num));
+		//message_text.push_str(&format!(" ({} votes)", num));
 		message_text.push('\n');
 	}
 
