0f5665cc0c680c75c4b812277649305572f4b2c7
quaoz <quaoz@protonmail.com>
2022-04-05 12:35:02 +0100

    rustfmt

--- src/commands/games/squirdle.rs
+++ src/commands/games/squirdle.rs
@@ -8,19 +8,19 @@ use serenity::prelude::*;
 #[usage("squirdle")]
 #[example("squirdle")]
 async fn squirdle(ctx: &Context, msg: &Message) -> CommandResult {
-    // Gets a json response from the url
-    let resp = reqwest::get("https://squirdle.fireblend.com/data/daily.json").await?;
-    let json: serde_json::Value = resp.json().await?;
+	// Gets a json response from the url
+	let resp = reqwest::get("https://squirdle.fireblend.com/data/daily.json").await?;
+	let json: serde_json::Value = resp.json().await?;
 
-    // Gets the current date in the format YYYY-MM-DD
-    let date = chrono::Local::now().format("%Y-%m-%d").to_string();
+	// Gets the current date in the format YYYY-MM-DD
+	let date = chrono::Local::now().format("%Y-%m-%d").to_string();
 
-    let content = format!(
-        "The daily squirdle for {} is ||{}||",
-        &date,
-        json[&date].get(1).unwrap()
-    );
-    msg.reply(&ctx.http, content).await?;
+	let content = format!(
+		"The daily squirdle for {} is ||{}||",
+		&date,
+		json[&date].get(1).unwrap()
+	);
+	msg.reply(&ctx.http, content).await?;
 
-    Ok(())
+	Ok(())
 }
